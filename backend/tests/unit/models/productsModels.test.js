edd63101701ab706857a0c8ec5fac447233fe75b
Bruno <bruno_bury@hotmail.com>
2023-09-19 16:59:58 -0300

    test2

--- backend/tests/unit/models/productsModels.test.js
+++ backend/tests/unit/models/productsModels.test.js
@@ -1,39 +1,39 @@
-const chai = require('chai');
-const sinon = require('sinon');
-const { productModels } = require('../../../src/models');
-const { productsMock, productById } = require('../mocks/mockProducts');
+// const chai = require('chai');
+// const sinon = require('sinon');
+// const { productModels } = require('../../../src/models');
+// const { productsMock, productById } = require('../mocks/mockProducts');
 
-const { expect } = chai;
+// const { expect } = chai;
 
-describe('Product Models', function () {
-  let connectiondb;
+// describe('Product Models', function () {
+//   let connectiondb;
 
-  beforeEach(function () {
-    connectiondb = {
-      execute: sinon.stub(),
-    };
-  });
+//   beforeEach(function () {
+//     connectiondb = {
+//       execute: sinon.stub(),
+//     };
+//   });
 
-  afterEach(function () {
-    sinon.restore();
-  });
+//   afterEach(function () {
+//     sinon.restore();
+//   });
 
-  describe('getAllModels', function () {
-    it('retorna todos produtos', async function () {
-      connectiondb.execute.resolves([productsMock]);
+//   describe('getAllModels', function () {
+//     it('retorna todos produtos', async function () {
+//       connectiondb.execute.resolves([productsMock]);
 
-      const result = await productModels.getAllModels(connectiondb);
-      expect(result).to.deep.equal(productsMock);
-    });
-  });
+//       const result = await productModels.getAllModels(connectiondb);
+//       expect(result).to.deep.equal(productsMock);
+//     });
+//   });
 
-  describe('getByIdModels', function () {
-    it('retorna produto pelo id', async function () {
-      connectiondb.execute.resolves([productById]);
+//   describe('getByIdModels', function () {
+//     it('retorna produto pelo id', async function () {
+//       connectiondb.execute.resolves([productById]);
 
-      const id = 1;
-      const result = await productModels.getByIdModels(id, connectiondb);
-      expect(result).to.deep.equal(productById);
-    });
-  });
-});
+//       const id = 1;
+//       const result = await productModels.getByIdModels(id, connectiondb);
+//       expect(result).to.deep.equal(productById);
+//     });
+//   });
+// });
