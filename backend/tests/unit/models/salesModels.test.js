b227314c9ec712623518b9ae4b131e0fdc770994
Bruno <bruno_bury@hotmail.com>
2023-09-19 16:25:56 -0300

    test

--- backend/tests/unit/models/salesModels.test.js
+++ backend/tests/unit/models/salesModels.test.js
@@ -1,38 +1,38 @@
-const chai = require('chai');
-const sinon = require('sinon');
-const { salesModels } = require('../../../src/models');
-const { salesMock, saleById } = require('../mocks/mockSales');
+// const chai = require('chai');
+// const sinon = require('sinon');
+// const { salesModels } = require('../../../src/models');
+// const { salesMock, saleById } = require('../mocks/mockSales');
 
-const { expect } = chai;
+// const { expect } = chai;
 
-describe('Sales Models', function () {
-  describe('getAllModels', function () {
-    it('deve retornar um array de vendas', async function () {
-      const getAllModelsStub = sinon.stub(salesModels, 'getAllModels');
-      getAllModelsStub.returns(salesMock);
+// describe('Sales Models', function () {
+//   describe('getAllModels', function () {
+//     it('deve retornar um array de vendas', async function () {
+//       const getAllModelsStub = sinon.stub(salesModels, 'getAllModels');
+//       getAllModelsStub.returns(salesMock);
 
-      const sales = await salesModels.getAllModels();
+//       const sales = await salesModels.getAllModels();
 
-      chai.assert.isTrue(getAllModelsStub.calledOnce);
+//       chai.assert.isTrue(getAllModelsStub.calledOnce);
 
-      expect(sales).to.deep.equal(salesMock);
+//       expect(sales).to.deep.equal(salesMock);
 
-      getAllModelsStub.restore();
-    });
-  });
+//       getAllModelsStub.restore();
+//     });
+//   });
 
-  describe('getByIdModels', function () {
-    it('deve retornar uma única venda por ID', async function () {
-      const getByIdModelsStub = sinon.stub(salesModels, 'getByIdModels');
-      getByIdModelsStub.withArgs(1).returns(saleById);
+//   describe('getByIdModels', function () {
+//     it('deve retornar uma única venda por ID', async function () {
+//       const getByIdModelsStub = sinon.stub(salesModels, 'getByIdModels');
+//       getByIdModelsStub.withArgs(1).returns(saleById);
 
-      const sale = await salesModels.getByIdModels(1);
+//       const sale = await salesModels.getByIdModels(1);
 
-      chai.assert.isTrue(getByIdModelsStub.calledOnceWith(1));
+//       chai.assert.isTrue(getByIdModelsStub.calledOnceWith(1));
 
-      expect(sale).to.deep.equal(saleById);
+//       expect(sale).to.deep.equal(saleById);
 
-      getByIdModelsStub.restore();
-    });
-  });
-});
+//       getByIdModelsStub.restore();
+//     });
+//   });
+// });
